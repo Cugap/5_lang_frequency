eb5c552923d5279ddbbb6f4f21ad513bed9cedb5
Ruslan <rusiacugap@mail.ru>
2018-03-20 20:25:38 +0300

    PEP8 исправления

--- lang_frequency.py
+++ lang_frequency.py
@@ -12,27 +12,30 @@ def load_data(filepath):
         print(ex)
     return text
 
+
 def get_words_from_text(text):
-    if text == None or len(text) == 0:
+    if text is None or len(text) == 0:
         return text
     text = text.lower()
     regex = re.compile(r"[^a-zа-я]", re.IGNORECASE)
     text = re.sub(regex, ' ', text)
-    
+
     return [x for x in re.split(" +", text) if x]
 
+
 def get_most_frequent_words(text):
-    if text == None or len(text) == 0:
+    if text is None or len(text) == 0:
         return
     words = get_words_from_text(text)
     counter = Counter(words)
-    
+
     table = Texttable()
     table.add_row(["Word", "Number uf uses"])
     for word, cnt in counter.items():
         table.add_row([word, cnt])
 
     print(table.draw())
+
 
 if __name__ == "__main__":
     print("Please input path for analized file")
